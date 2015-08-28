f86b019d55448126f00895b82c32a3c62c4f344e
ainduk <ainduk@DHP50E683.dhp.ad.deanhealth.com>
2015-08-28 15:59:45 -0500

    Menu Continued... 0828:0400

--- includes/html_codes.php
+++ includes/html_codes.php
@@ -4,24 +4,47 @@
 function headerAndSearchCode(){
     echo "
 			<div class='row'>
-    			<div class='col-sm-1'></div>
-	  			<div class='col-sm-1'><img src=\"C:/xampp/htdocs/ProjectX/images/home.png\"></div>
+    			<div class='col-sm-1' id='cssTopRightLinks'></div>
+	  			<div class='col-sm-1'>
+    				<div id='cssTopLeftLogo'>
+    					<img src=\"C:/xampp/htdocs/ProjectX/images/home.png\">
+    				</div>
+    			</div>
 	  			<div class='col-sm-6'>
-    				<div class='btn-group btn-group-lg'>
-	    				<a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>Buy</a>
-					    <a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>Sell</a>
-					    <a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>Rent</a>
-					    <a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>Home Loans</a>
-					    <a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>Find an Agent</a>
-					    <a href='#' class='btn btn-primary' title='Header' data-toggle='popover' data-trigger='hover' data-placement='bottom' data-content='#'>More</a>
+    				<div class='btn-group btn-group-lg' id='cssMenu'>
+	    				<a href='#' class='btn btn-primary' id='buyDataToggle' data-trigger='hover' data-placement='bottom'>Buy</a>
+    						<div id='buyPopoverContent' class='hide'>
+						      Here goes content for buy
+						    </div>
+					    <a href='#' class='btn btn-primary' id='sellDataToggle' data-trigger='hover' data-placement='bottom'>Sell</a>
+    						<div id='sellPopoverContent' class='hide'>
+						      Here goes content for Sell
+						    </div>
+					    <a href='#' class='btn btn-primary' id='rentDataToggle' data-trigger='hover' data-placement='bottom'>Rent</a>
+    						<div id='rentPopoverContent' class='hide'>
+						      Here goes content for Rent
+						    </div>
+					    <a href='#' class='btn btn-primary' id='loansDataToggle' data-trigger='hover' data-placement='bottom'>Home Loans</a>
+    						<div id='loansPopoverContent' class='hide'>
+						      Here goes content for Home Loans
+						    </div>
+					    <a href='#' class='btn btn-primary' id='agentDataToggle' data-trigger='hover' data-placement='bottom'>Find an Agent</a>
+    						<div id='agentPopoverContent' class='hide'>
+						      Here goes content for Find an Agent
+						    </div>
+					    <a href='#' class='btn btn-primary' id='newsDataToggle' data-trigger='hover' data-placement='bottom'>News & Advise</a>
+    						<div id='newsPopoverContent' class='hide'>
+						      Here goes content for News & Advise
+						    </div>
 				    </div>
     		    </div>
-    			<div class='col-sm-4'>
+    			<div class='col-sm-2' id='cssTopRightLinks'>
     		";
     		topRightLinks();
-			echo '</div>
+			echo "</div>
+				<div class='col-sm-2' id='cssTopRightLinks'></div>
 			  </div>
-    		';
+    		";
 }
 
 //Code for Footer bar
--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,5 +1,45 @@
 
 // Select all elements with data-toggle="popover" in the document
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover(); 
+	$('#buyDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#buyPopoverContent').html();
+	        }
+	});
+	
+	$('#sellDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#sellPopoverContent').html();
+	        }
+	});
+	
+	$('#rentDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#rentPopoverContent').html();
+	        }
+	});
+	
+	$('#loansDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#loansPopoverContent').html();
+	        }
+	});
+	
+	$('#agentDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#agentPopoverContent').html();
+	        }
+	});
+	
+	$('#newsDataToggle').popover({
+	    html: true, 
+		content: function() {
+	          return $('#newsPopoverContent').html();
+	        }
+	});
 });
